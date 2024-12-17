1a0ac54b63f378106e39103dc1b5fe8d5b8fc8b1
laurhakob <laurhakob@gmail.com>
2024-12-17 23:01:36 +0400

    header & navigation, custom animations with tailwind, light & dark mode shadcn

--- src/app/(rs)/layout.tsx
+++ src/app/(rs)/layout.tsx
@@ -1,3 +1,4 @@
+import { Header } from "@/components/Header";
 import React from "react";
 
 export default async function RSLayout({
@@ -7,7 +8,7 @@ export default async function RSLayout({
 }) {
   return (
     <div className="mx-auto w-full max-w-7xl">
-      {/* Header */}
+      <Header />
       <div className="px-4 py-2">{children}</div>
     </div>
   );
