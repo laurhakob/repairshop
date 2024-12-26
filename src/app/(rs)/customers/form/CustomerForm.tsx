82fb5e5250e1d691ff17854491818b5c9f2f0aef
laurhakob <laurhakob@gmail.com>
2024-12-26 13:50:45 +0400

    server actions with next-safe-action

--- src/app/(rs)/customers/form/CustomerForm.tsx
+++ src/app/(rs)/customers/form/CustomerForm.tsx
@@ -4,18 +4,27 @@ import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Form } from "@/components/ui/form";
 import { Button } from "@/components/ui/button";
+
 import { InputWithLabel } from "@/components/inputs/InputWithLabel";
 import { SelectWithLabel } from "@/components/inputs/SelectWithLabel";
 import { TextAreaWithLabel } from "@/components/inputs/TextAreaWithLabel";
-import { useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
 import { CheckboxWithLabel } from "@/components/inputs/CheckboxWithLabel";
+
+import { useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
+
+import { StatesArray } from "@/constants/StatesArray";
 
 import {
   insertCustomerSchema,
   type insertCustomerSchemaType,
   type selectCustomerSchemaType,
 } from "@/zod-schemas/customer";
-import { StatesArray } from "@/constants/StatesArray";
+
+import { useAction } from "next-safe-action/hooks";
+import { saveCustomerAction } from "@/app/actions/saveCustomerAction";
+import { useToast } from "@/hooks/use-toast";
+import { LoaderCircle } from "lucide-react";
+import { DisplayServerActionResponse } from "@/components/DisplayServerActionResponse";
 
 type Props = {
   customer?: selectCustomerSchemaType;
@@ -24,6 +33,8 @@ type Props = {
 export default function CustomerForm({ customer }: Props) {
   const { getPermission, isLoading } = useKindeBrowserClient();
   const isManager = !isLoading && getPermission("manager")?.isGranted;
+
+  const { toast } = useToast();
 
   const defaultValues: insertCustomerSchemaType = {
     id: customer?.id ?? 0,
@@ -46,12 +57,37 @@ export default function CustomerForm({ customer }: Props) {
     defaultValues,
   });
 
+  const {
+    execute: executeSave,
+    result: saveResult,
+    isPending: isSaving,
+    reset: resetSaveAction,
+  } = useAction(saveCustomerAction, {
+    onSuccess({ data }) {
+      if (data?.message) {
+        toast({
+          variant: "default",
+          title: "Success! 🎉",
+          description: data.message,
+        });
+      }
+    },
+    onError({ error }) {
+      toast({
+        variant: "destructive",
+        title: "Error",
+        description: "Save Failed",
+      });
+    },
+  });
+
   async function submitForm(data: insertCustomerSchemaType) {
-    console.log(data);
+    executeSave(data);
   }
 
   return (
     <div className="flex flex-col gap-1 sm:px-8">
+      <DisplayServerActionResponse result={saveResult} />
       <div>
         <h2 className="text-2xl font-bold">
           {customer?.id ? "Edit" : "New"} Customer{" "}
@@ -83,6 +119,7 @@ export default function CustomerForm({ customer }: Props) {
               fieldTitle="Address 2"
               nameInSchema="address2"
             />
+
             <InputWithLabel<insertCustomerSchemaType>
               fieldTitle="City"
               nameInSchema="city"
@@ -100,10 +137,12 @@ export default function CustomerForm({ customer }: Props) {
               fieldTitle="Zip Code"
               nameInSchema="zip"
             />
+
             <InputWithLabel<insertCustomerSchemaType>
               fieldTitle="Email"
               nameInSchema="email"
             />
+
             <InputWithLabel<insertCustomerSchemaType>
               fieldTitle="Phone"
               nameInSchema="phone"
@@ -131,15 +170,25 @@ export default function CustomerForm({ customer }: Props) {
                 className="w-3/4"
                 variant="default"
                 title="Save"
+                disabled={isSaving}
               >
-                Save
+                {isSaving ? (
+                  <>
+                    <LoaderCircle className="animate-spin" /> Saving
+                  </>
+                ) : (
+                  "Save"
+                )}
               </Button>
 
               <Button
                 type="button"
                 variant="destructive"
                 title="Reset"
-                onClick={() => form.reset(defaultValues)}
+                onClick={() => {
+                  form.reset(defaultValues);
+                  resetSaveAction();
+                }}
               >
                 Reset
               </Button>
--- src/app/(rs)/tickets/form/TicketForm.tsx
+++ src/app/(rs)/tickets/form/TicketForm.tsx
@@ -6,8 +6,9 @@ import { Form } from "@/components/ui/form";
 import { Button } from "@/components/ui/button";
 
 import { InputWithLabel } from "@/components/inputs/InputWithLabel";
-// import { SelectWithLabel } from "@/components/inputs/SelectWithLabel"
+import { SelectWithLabel } from "@/components/inputs/SelectWithLabel";
 import { TextAreaWithLabel } from "@/components/inputs/TextAreaWithLabel";
+import { CheckboxWithLabel } from "@/components/inputs/CheckboxWithLabel";
 
 import {
   insertTicketSchema,
@@ -15,8 +16,12 @@ import {
   type selectTicketSchemaType,
 } from "@/zod-schemas/ticket";
 import { selectCustomerSchemaType } from "@/zod-schemas/customer";
-import { CheckboxWithLabel } from "@/components/inputs/CheckboxWithLabel";
-import { SelectWithLabel } from "@/components/inputs/SelectWithLabel";
+
+import { useAction } from "next-safe-action/hooks";
+import { saveTicketAction } from "@/app/actions/saveTicketAction";
+import { useToast } from "@/hooks/use-toast";
+import { LoaderCircle } from "lucide-react";
+import { DisplayServerActionResponse } from "@/components/DisplayServerActionResponse";
 
 type Props = {
   customer: selectCustomerSchemaType;
@@ -36,6 +41,8 @@ export default function TicketForm({
 }: Props) {
   const isManager = Array.isArray(techs);
 
+  const { toast } = useToast();
+
   const defaultValues: insertTicketSchemaType = {
     id: ticket?.id ?? "(New)",
     customerId: ticket?.customerId ?? customer.id,
@@ -51,15 +58,44 @@ export default function TicketForm({
     defaultValues,
   });
 
+  const {
+    execute: executeSave,
+    result: saveResult,
+    isPending: isSaving,
+    reset: resetSaveAction,
+  } = useAction(saveTicketAction, {
+    onSuccess({ data }) {
+      if (data?.message) {
+        toast({
+          variant: "default",
+          title: "Success! 🎉",
+          description: data.message,
+        });
+      }
+    },
+    onError({ error }) {
+      toast({
+        variant: "destructive",
+        title: "Error",
+        description: "Save Failed",
+      });
+    },
+  });
+
   async function submitForm(data: insertTicketSchemaType) {
-    console.log(data);
+    executeSave(data);
   }
 
   return (
     <div className="flex flex-col gap-1 sm:px-8">
+      <DisplayServerActionResponse result={saveResult} />
       <div>
         <h2 className="text-2xl font-bold">
-          {ticket?.id && isEditable ? `Edit Ticket # ${ticket.id}` : ticket?.id ? `View Ticket # ${ticket.id}` : "New Ticket Form"}
+          {ticket?.id && isEditable
+            ? `Edit Ticket # ${ticket.id}`
+            : ticket?.id
+            ? `View Ticket # ${ticket.id}`
+            : "New Ticket Form"}
         </h2>
       </div>
       <Form {...form}>
@@ -135,15 +171,25 @@ export default function TicketForm({
                   className="w-3/4"
                   variant="default"
                   title="Save"
+                  disabled={isSaving}
                 >
-                  Save
+                  {isSaving ? (
+                    <>
+                      <LoaderCircle className="animate-spin" /> Saving
+                    </>
+                  ) : (
+                    "Save"
+                  )}
                 </Button>
 
                 <Button
                   type="button"
                   variant="destructive"
                   title="Reset"
-                  onClick={() => form.reset(defaultValues)}
+                  onClick={() => {
+                    form.reset(defaultValues);
+                    resetSaveAction();
+                  }}
                 >
                   Reset
                 </Button>
